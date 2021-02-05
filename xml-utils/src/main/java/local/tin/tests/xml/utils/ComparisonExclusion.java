3766a3d34d525cbf451acc82160fcf6ddc53ba72
benitodarder@gmail.com <benitodarder@gmail.com>
2021-02-05 13:43:28 +0100

    Model class comparison exclusion

---
+++ xml-utils/src/main/java/local/tin/tests/xml/utils/ComparisonExclusion.java
@@ -0,0 +1,39 @@
+package local.tin.tests.xml.utils;
+
+/**
+ *
+ * @author benitodarder
+ */
+public class ComparisonExclusion {
+    
+    private String parentLocalName;
+    private String nodeLocalName;
+    private String attributeName;
+
+    public String getParentLocalName() {
+        return parentLocalName;
+    }
+
+    public void setParentLocalName(String parentLocalName) {
+        this.parentLocalName = parentLocalName;
+    }
+
+    public String getNodeLocalName() {
+        return nodeLocalName;
+    }
+
+    public void setNodeLocalName(String nodeLocalName) {
+        this.nodeLocalName = nodeLocalName;
+    }
+
+    public String getAttributeName() {
+        return attributeName;
+    }
+
+    public void setAttributeName(String attributeName) {
+        this.attributeName = attributeName;
+    }
+    
+    
+    
+}
